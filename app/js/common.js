422ecc934ccda5d3f10d9d67bf7b6c0d31a65c58
Viktoria <zaharova181114@gmail.com>
2021-10-13 22:22:33 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,26 @@
+$('.news-slider').slick({
+    slidesToShow: 2,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 650,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+$('[name="phone"]').mask('+7 (999) 999-99-99');
+
+$('.btn-burger').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
+
+new WOW().init();
